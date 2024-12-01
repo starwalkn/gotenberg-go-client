c945a22ef4cabae6709ec06c3c39ca8f84064c37
Alexander Pikeev <alexanderpikeev@xmail.ru>
2024-12-01 12:07:23 +0300

    fix: add new user in container for tests

--- build/tests/docker-entrypoint.sh
+++ build/tests/docker-entrypoint.sh
@@ -6,5 +6,5 @@ set -xe
 gotenberg --api-enable-basic-auth --chromium-auto-start &
 sleep 10
 export CGO_ENABLED=1
-go test -race -cover -covermode=atomic github.com/runatal/gotenberg-go-client/v8
+go test -v -race -cover -covermode=atomic ./...
 sleep 10 # allows Gotenberg to remove generated files.
